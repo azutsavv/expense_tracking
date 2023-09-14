3545894538be2396d3b8e4617b7e6e3ef772005b
Utsav Singh <azutsav2002@gmail.com>
2023-09-14 22:24:51 +0530

    modal page working and adding widgets to it

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -7,9 +7,38 @@ class NewExpense extends StatefulWidget {
   State<NewExpense> createState() => _NewExpenseState();
 }
 
+var _enteredTitle = '';
+
+void _savetitleinnput(String inputValue) {
+  _enteredTitle = inputValue;
+}
+
 class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding:const  EdgeInsets.all(16),
+      child: Column(
+        children: [
+          const TextField(
+            maxLength: 50,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              label: Text("title"),
+            ),
+            onChanged: _savetitleinnput,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_enteredTitle);
+                  },
+                  child:const Text("SAVE")),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
