b4e38f1b18db935dde73c82af9c83739469be9d1
Utsav Singh <azutsav2002@gmail.com>
2023-09-17 16:27:10 +0530

    color schema is changed using with copyfrom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var KcolorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 70, 5, 175));
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,18 +11,45 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme:  ThemeData(useMaterial3: true),
+      theme:
+          ThemeData().copyWith(
+            useMaterial3: true, 
+            colorScheme: KcolorScheme,
+            appBarTheme: AppBarTheme(
+              backgroundColor: KcolorScheme.onPrimaryContainer,
+              foregroundColor: KcolorScheme.primaryContainer,
+            ),
+
+            cardTheme: CardTheme(
+              color: KcolorScheme.secondaryContainer,
+              margin:const EdgeInsets.symmetric(horizontal: 16 , vertical: 8),
+              elevation: 10,
+              shadowColor: KcolorScheme.primaryContainer,
+            ),
+
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: KcolorScheme.primaryContainer,
+              )
+            ),
+
+            textTheme: ThemeData().textTheme.copyWith(
+              
+            )    
+
+            
+            
+            
+            ),
+
       initialRoute: '/expense',
       routes: {
-        '/expense':(context) => const Expenses(),
+        '/expense': (context) => const Expenses(),
       },
-      
     );
   }
 }
-
