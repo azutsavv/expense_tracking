e76858fe9e1fb023dfaabe72cf07ecf80cc4d6b4
Utsav Singh <azutsav2002@gmail.com>
2023-09-17 20:22:04 +0530

    dark mode funtionality done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 
 var KcolorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 70, 5, 175));
+var KDarkColorScheme =
+    ColorScheme.fromSeed(
+      brightness: Brightness.dark ,
+      seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
   runApp(const MyApp());
@@ -15,41 +19,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData().copyWith(
-            useMaterial3: true, 
-            colorScheme: KcolorScheme,
-            appBarTheme: AppBarTheme(
-              backgroundColor: KcolorScheme.onPrimaryContainer,
-              foregroundColor: KcolorScheme.primaryContainer,
-            ),
 
-            cardTheme: CardTheme(
-              color: KcolorScheme.secondaryContainer,
-              margin:const EdgeInsets.symmetric(horizontal: 16 , vertical: 8),
-              elevation: 10,
-              shadowColor: KcolorScheme.primaryContainer,
-            ),
+      
+      darkTheme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: KDarkColorScheme,
 
-            elevatedButtonTheme: ElevatedButtonThemeData(
+       scaffoldBackgroundColor: KDarkColorScheme.onSecondaryContainer,
+        appBarTheme: AppBarTheme(
+            backgroundColor: KDarkColorScheme.primaryContainer,
+            foregroundColor: KDarkColorScheme.onPrimaryContainer,
+          ),
+          cardTheme: CardTheme(
+            color: KDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            elevation: 10,
+            shadowColor: KDarkColorScheme.primaryContainer,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
               style: ElevatedButton.styleFrom(
-                backgroundColor: KcolorScheme.primaryContainer,
+            backgroundColor: KDarkColorScheme.primaryContainer,
+            foregroundColor: KDarkColorScheme.onPrimaryContainer,
+          )),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  color: KDarkColorScheme.onSecondaryContainer,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
               )
-            ),
+      ),
 
-            textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                color: KcolorScheme.onSecondaryContainer,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
-              ),
-            )    
 
-            
-            
-            
-            ),
+
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: KcolorScheme,
+          appBarTheme: AppBarTheme(
+            backgroundColor: KcolorScheme.onPrimaryContainer,
+            foregroundColor: KcolorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme(
+            color: KcolorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            elevation: 10,
+            shadowColor: KcolorScheme.primaryContainer,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: KcolorScheme.primaryContainer,
+          )),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  color: KcolorScheme.onSecondaryContainer,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              )),
+      themeMode: ThemeMode.system ,
+
 
       initialRoute: '/expense',
       routes: {
