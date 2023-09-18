18b790726bdf00dde9e7196230c72d1c6192dc3f
Utsav Singh <azutsav2002@gmail.com>
2023-09-18 21:02:54 +0530

    chart bar added with dark mode compatiblity, now start working on responsiveness and adapttation

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -83,7 +84,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('chart'),
+          Chart(expenses: _registeredExpense),
           Expanded(child: mainContent),
         ],
       ),
